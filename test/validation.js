4e7529347e477c4e598a726eb0a7b30857399c54
sle <sle@salesforce.com>
2015-11-25 11:56:57 -0800

    first reasonable test

--- test/validation.js
+++ test/validation.js
@@ -1,10 +1,24 @@
-var assert = require("assert");
-var fs = require('fs');
-var path = require('path');
-var _ = require('lodash');
+import {assert} from 'chai';
+import _ from 'lodash';
 
-describe('Aura Explorer', function() {
-    it('should pass', function() {
-        assert.equal(true, true);
-    });
+//internal
+import dataDependenciesMap from '../data/dependenciesMap.json';
+import usageMaps from '../data/usageMap.json';
+import controlCountMap from '../data/controlCountMap.json';
+import namespaceCountMap from '../data/namespaceCountMap.json';
+import autoCompleteControlMap from '../data/autoCompleteControlMap.json';
+import controlLocationMap from '../data/controlLocationMap.json';
+
+
+describe('Aura Explorer', () => {
+    describe('Meta Data Test', () => {
+    	it('should have some meta data', () => {
+	        assert.isTrue( _.size(dataDependenciesMap) > 0, 'dataDependenciesMap is empty');
+			assert.isTrue( _.size(usageMaps) > 0, 'usageMaps is empty');
+			assert.isTrue( _.size(controlCountMap) > 0, 'controlCountMap is empty');
+			assert.isTrue( _.size(namespaceCountMap) > 0, 'namespaceCountMap is empty');
+			assert.isTrue( _.size(autoCompleteControlMap) > 0, 'autoCompleteControlMap is empty');
+			assert.isTrue( _.size(controlLocationMap) > 0, 'controlLocationMap is empty');
+	    });
+    })
 })
