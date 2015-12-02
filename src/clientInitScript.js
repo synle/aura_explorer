2bced4b4248a4a74ea2b371d968f5e61541cda8d
sle <sle@salesforce.com>
2015-12-02 15:57:00 -0800

    promisfy all front end code

--- src/clientInitScript.js
+++ src/clientInitScript.js
@@ -1,6 +1,7 @@
 //import
 var path = require( 'path' );
 var fs = require( 'fs' );
+var Q = require( 'q' );
 
 //polyfill
 //this script is appended to vendor script to make certain external
@@ -9,6 +10,20 @@ global.React = React;
 global.$ = $;
 global.util_readFromFileAsync = function (fpath) {
 	var fullPath = path.join(process.cwd(), fpath);	
-	var content = fs.readFileSync(fullPath, 'utf8');
-	return content;
+	var defer = Q.defer();
+
+    try{
+        fs.readFile(fullPath, 'utf-8', function (error, fileContent) {
+            if (error){
+                throw error;
+            } else {
+                defer.resolve(fileContent);
+            }
+        });
+    } catch(exception) {
+        console.log('global.util_readFromFileAsync error', exception);
+        defer.resolve('');//if there is error, resolve with an empty file
+    }
+
+    return defer.promise;
 };
--- src/frontend/library/restClient.js
+++ src/frontend/library/restClient.js
@@ -5,65 +5,36 @@ import path from 'path';
 //internal
 const util_getJoinPath = (myPath) => myPath;//path.join( process.cwd(), myPath );
 
-//get contents
-const auraExplorerJson = global.util_readFromFileAsync(
-	util_getJoinPath( 'package.json' )
-);
-
-const auraStreamPom    = global.util_readFromFileAsync(
-	util_getJoinPath( 'public/dist/js/data/aura_upstream_pom.xml' )
-);
-
-const dataDependenciesMap = JSON.parse( global.util_readFromFileAsync( 'public/dist/js/data/dependenciesMap.json' ) );
-const usageMap = JSON.parse( global.util_readFromFileAsync( 'public/dist/js/data/usageMap.json' ) );
-const controlCountMap = JSON.parse( global.util_readFromFileAsync( 'public/dist/js/data/controlCountMap.json' ) );
-const namespaceCountMap = JSON.parse( global.util_readFromFileAsync( 'public/dist/js/data/namespaceCountMap.json' ) );
-const explorerConfig = {};
-
-const packageInfo = JSON.parse(auraExplorerJson);
+const _parsePromise = stringContent => JSON.parse(stringContent);
 
 
 //definitions
 const restClient = {
-	getDataDependenciesMap(){
-		const defer = Q.defer();
-		defer.resolve(dataDependenciesMap);
-		return defer.promise;
-	},
-	getUsageMap(){
-		const defer = Q.defer();
-		defer.resolve(usageMap);
-		return defer.promise;
-	},
-	getControlCountMap(){
-		const defer = Q.defer();
-		defer.resolve(controlCountMap);
-		return defer.promise;
-	},
-	getNamespaceCountMap(){
-		const defer = Q.defer();
-		defer.resolve(namespaceCountMap);
-		return defer.promise;
-	},
 	getPackageInfo(){
-		const defer = Q.defer();
-		defer.resolve(packageInfo);
-		return defer.promise;
-	},
-	getExplorerConfig(){
-		const defer = Q.defer();
-		defer.resolve(explorerConfig);
-		return defer.promise;
+		return global.util_readFromFileAsync( util_getJoinPath( 'package.json' ) );
 	},
 	getAuraExplorerJson(){
-		const defer = Q.defer();
-		defer.resolve(auraExplorerJson);
-		return defer.promise;
+		return global.util_readFromFileAsync( util_getJoinPath( 'package.json' ) )
+			.then(_parsePromise);
+	},
+	getDataDependenciesMap(){
+		return global.util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/dependenciesMap.json' ) )
+			.then(_parsePromise);
+	},
+	getUsageMap(){
+		return global.util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/usageMap.json' ) )
+			.then(_parsePromise);
+	},
+	getControlCountMap(){
+		return global.util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/controlCountMap.json' ) )
+			.then(_parsePromise);
+	},
+	getNamespaceCountMap(){
+		return global.util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/namespaceCountMap.json' ) )
+			.then(_parsePromise);
 	},
 	getAuraStreamPom(){
-		const defer = Q.defer();
-		defer.resolve(auraStreamPom);
-		return defer.promise;
+		return global.util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/aura_upstream_pom.xml' ) );
 	}
 }
 
