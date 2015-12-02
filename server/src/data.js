a1a9cf6d55fe511e849936bbed3e3249d1b5140f
sle <sle@salesforce.com>
2015-12-02 15:58:07 -0800

    server code clean up

--- server/src/data.js
+++ server/src/data.js
@@ -7,7 +7,7 @@ import util from './util';
 
 //get path
 const util_getJoinPath = (myPath) => `../${myPath}`;//path.join( process.cwd(), myPath );
-
+const util_readFromFileAsync = util.readFromFileAsync;
 
 const _parsePromise = stringContent => JSON.parse(stringContent);
 
@@ -15,30 +15,30 @@ const _parsePromise = stringContent => JSON.parse(stringContent);
 //definitions
 const restClient = {
 	getPackageInfo(){
-		return util.readFromFileAsync( util_getJoinPath( 'package.json' ) );
+		return util_readFromFileAsync( util_getJoinPath( 'package.json' ) );
 	},
 	getAuraExplorerJson(){
-		return util.readFromFileAsync( util_getJoinPath( 'package.json' ) )
+		return util_readFromFileAsync( util_getJoinPath( 'package.json' ) )
 			.then(_parsePromise);
 	},
 	getDataDependenciesMap(){
-		return util.readFromFileAsync( util_getJoinPath( 'public/dist/js/data/dependenciesMap.json' ) )
+		return util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/dependenciesMap.json' ) )
 			.then(_parsePromise);
 	},
 	getUsageMap(){
-		return util.readFromFileAsync( util_getJoinPath( 'public/dist/js/data/usageMap.json' ) )
+		return util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/usageMap.json' ) )
 			.then(_parsePromise);
 	},
 	getControlCountMap(){
-		return util.readFromFileAsync( util_getJoinPath( 'public/dist/js/data/controlCountMap.json' ) )
+		return util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/controlCountMap.json' ) )
 			.then(_parsePromise);
 	},
 	getNamespaceCountMap(){
-		return util.readFromFileAsync( util_getJoinPath( 'public/dist/js/data/namespaceCountMap.json' ) )
+		return util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/namespaceCountMap.json' ) )
 			.then(_parsePromise);
 	},
 	getAuraStreamPom(){
-		return util.readFromFileAsync( util_getJoinPath( 'public/dist/js/data/aura_upstream_pom.xml' ) );
+		return util_readFromFileAsync( util_getJoinPath( 'public/dist/js/data/aura_upstream_pom.xml' ) );
 	}
 }
 
