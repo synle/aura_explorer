6b26dd1d9fcc60c2a03cc8d7c79d9c582f5fb2d3
sle <sle@salesforce.com>
2015-12-12 19:15:54 -0800

    added test for meta data from sqlite

--- test/validation.js
+++ test/validation.js
@@ -1,11 +1,19 @@
+'use strict';
+
+//external
 import {assert} from 'chai';
 import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
+import Q from 'q';
+
+//internal
 import CONFIG from '~/config.json';
+import metaDataService from '~/src/common/model/metaData';
 
 const _readFile = (fpath) => {
 	const fullPath = path.join(process.cwd(), CONFIG.auraMetaOutputDir, `${fpath}`);
+	// const fullPath = path.join(process.cwd(), CONFIG.auraMetaOutputDir, 'latest', `${fpath}`);
 	console.log(fullPath);
 	return fs.readFileSync(
 		fullPath,
@@ -31,7 +39,7 @@ const controlLocationMap = _readJsonFile('controlLocationMap.json');
 
 
 describe('Aura Explorer', () => {
-    describe('Meta Data Test', () => {
+    describe('MetaData Plain Files Test', () => {
     	it('should have some meta data', () => {
 	        assert.isTrue( _.size(dataDependenciesMap) > 0, 'dataDependenciesMap is empty');
 			assert.isTrue( _.size(usageMaps) > 0, 'usageMaps is empty');
@@ -52,5 +60,64 @@ describe('Aura Explorer', () => {
 			// assert.isTrue( _.size(dataDependenciesMap.ui['scroller.cmp'].methods) > 0, 'dataDependenciesMap.ui.methods is empty');
 			assert.isTrue( _.size(dataDependenciesMap.ui['scroller.cmp'].dependencies) > 0, 'dataDependenciesMap.ui.dependencies is empty');
 	    });
-    })
+    });
+
+
+	describe('MetaData Database (SQLite3) Test', () => {
+		const promiseMetaDbInit = metaDataService.init();
+		const commitToValidate = 'latest';
+
+		const getMetaToTestPromise = () => {
+			const defer = Q.defer();
+
+			promiseMetaDbInit.then(() => {
+    			metaDataService.getMetaByCommitId( commitToValidate )
+	    			.then(
+	    				metaData => defer.resolve(metaData)
+					).catch(
+						e => {
+							console.log('metaDataService.getMetaByCommitId(latest): ', e.message)
+							assert.throw('metaDataService.getMetaByCommitId(latest): ' + e.message)
+						}
+					);
+			}).catch(
+				e => {
+					console.log('metaDataService.init(): ', e.message)
+					assert.throw('metaDataService.init(): ' + e.message)
+				}
+			);
+
+			return defer.promise;
+		}
+
+    	it('should have the latest', function(done){
+    		getMetaToTestPromise().then( metaData => {
+				assert.isAbove( _.keys( metaData ).length , 0, 'MetaData from SQLite should return something' );
+
+				assert.isDefined( metaData['aura_upstream_pom.xml'], 'aura_upstream_pom.xml defined' );
+				assert.isDefined( metaData['autocompletecontrolmap.json'], 'autocompletecontrolmap.json defined' );
+				assert.isDefined( metaData['controlcountmap.json'], 'controlcountmap.json defined' );
+				assert.isDefined( metaData['controllocationmap.json'], 'controllocationmap.json defined' );
+				assert.isDefined( metaData['dependenciesmap.json'], 'dependenciesmap.json defined' );
+				assert.isDefined( metaData['namespacecountmap.json'], 'namespacecountmap.json defined' );
+				assert.isDefined( metaData['usagemap.json'], 'usagemap.json defined' );
+
+				done();
+			}).catch( e => console.log(e));
+	    });
+
+
+	    it('dataDependenciesMap should have valid data', () => {
+	    	getMetaToTestPromise().then( metaData => {
+				assert.isDefined( metaData['aura_upstream_pom.xml'], 'aura_upstream_pom.xml defined' );
+				assert.isDefined( metaData['autocompletecontrolmap.json'], 'autocompletecontrolmap.json defined' );
+				assert.isDefined( metaData['controlcountmap.json'], 'controlcountmap.json defined' );
+				assert.isDefined( metaData['controllocationmap.json'], 'controllocationmap.json defined' );
+				assert.isDefined( metaData['dependenciesmap.json'], 'dependenciesmap.json defined' );
+				assert.isDefined( metaData['namespacecountmap.json'], 'namespacecountmap.json defined' );
+				assert.isDefined( metaData['usagemap.json'], 'usagemap.json defined' );
+				done();
+			}).catch( e => console.log(e));
+	    });
+    });
 })
